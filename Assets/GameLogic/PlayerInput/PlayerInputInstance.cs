81e6337ffdafa99153a12a138c673ccb9f978c57
Bezymyanyj <Bezymyanyj@gmail.com>
2021-12-04 18:03:38 -0500

    Player Input

---
+++ Assets/GameLogic/PlayerInput/PlayerInputInstance.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GameLogic.CameraController
+{
+    public class PlayerInputInstance : MonoBehaviour
+    {
+        public static PlayerInputInstance Instance;
+        public PlayerInput PlayerInput { get; private set; }
+        private void Awake()
+        {
+            PlayerInput = new PlayerInput();
+            PlayerInput.Enable();
+            
+            Instance = this;
+        }
+    }
+}
