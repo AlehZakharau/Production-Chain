0091f1f8ed8e88fe752a26d04e4ad1a5b5b23b09
Bezymyanyj <Bezymyanyj@gmail.com>
2021-12-04 18:02:25 -0500

    Refinery Resource Storage Models

---
+++ Assets/GameLogic/Buildings/Models/RefineryProduceStorageModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GameLogic.Manufacture
+{
+    public interface IRefineryProduceStorageModel
+    {
+        public Dictionary<ResourceType, int> ProduceResources { get; }
+
+        public bool AddResource(ResourceType resourceType);
+
+        public bool SpendResourceForCreateResource();
+    }
+    public class RefineryProduceStorageModel : IRefineryProduceStorageModel
+    {
+        private readonly List<ResourceType> demandProductionResources;
+        private readonly Dictionary<ResourceType, int> productionResources;
+
+        public Dictionary<ResourceType, int> ProduceResources => productionResources;
+
+        public RefineryProduceStorageModel(InitializeData.RefineryInitData initData)
+        {
+            demandProductionResources = initData.demandProducingResources;
+            
+            productionResources = new Dictionary<ResourceType, int>();
+            foreach (var resource in demandProductionResources)
+            {
+                productionResources.Add(resource, 0);
+            }
+        }
+
+        public bool AddResource(ResourceType resourceType)
+        {
+            if (demandProductionResources.Contains(resourceType))
+            {
+                productionResources[resourceType]++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool SpendResourceForCreateResource()
+        {
+            if (productionResources.Any(
+                resource => resource.Value < 1))
+            {
+                return false;
+            }
+            foreach (var varResource in demandProductionResources)
+            {
+                productionResources[varResource]--;
+            }
+            return true;
+        }
+    }
+}
---
+++ Assets/GameLogic/Buildings/Models/RefineryResourceStorageModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GameLogic.Manufacture
+{
+    public class RefineryResourceStorageModel : IResourceStorageModel
+    {
+        public event Action OnUpgrade;
+
+        private int resourceAmount;
+
+        private readonly IBuildingUpgraderModel buildingUpgraderModel;
+        private readonly IRefineryProduceStorageModel refineryProduceStorageModel;
+
+        public int ResourceAmount => resourceAmount < 1 ? 0 : resourceAmount;
+
+        public RefineryResourceStorageModel(IBuildingUpgraderModel buildingUpgraderModel,
+            IRefineryProduceStorageModel refineryProduceStorageModel)
+        {
+            this.buildingUpgraderModel = buildingUpgraderModel;
+            this.refineryProduceStorageModel = refineryProduceStorageModel;
+            buildingUpgraderModel.OnUpgrade += () => OnUpgrade?.Invoke();
+        }
+
+        public bool AddDemandResources(ResourceType resource)
+        {
+            if (buildingUpgraderModel.AddResource(resource))
+            {
+                return true;
+            }
+
+            if (refineryProduceStorageModel.AddResource(resource)) ;
+            {
+                return true;
+            }
+        }
+
+        public bool ProduceResource()
+        {
+            if (refineryProduceStorageModel.SpendResourceForCreateResource()) ;
+            {
+                resourceAmount++;
+                return true;
+            }
+        }
+    }
+}
