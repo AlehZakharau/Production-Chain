f8e4b219b18a950071d91edda80bf0e10d6fa24f
Bezymyanyj <Bezymyanyj@gmail.com>
2021-12-04 18:02:35 -0500

    Resource Storage Model

---
+++ Assets/GameLogic/Buildings/Models/ResourceStorageModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameLogic.Manufacture
+{
+    public class ResourceStorageModel : IResourceStorageModel
+    {
+        public event Action OnUpgrade;
+
+        private int resourceAmount;
+
+        private readonly IBuildingUpgraderModel buildingUpgraderModel;
+
+        public int ResourceAmount => resourceAmount < 1 ? 0 : resourceAmount;
+
+        public ResourceStorageModel(IBuildingUpgraderModel buildingUpgraderModel)
+        {
+            this.buildingUpgraderModel = buildingUpgraderModel;
+            buildingUpgraderModel.OnUpgrade += () => OnUpgrade?.Invoke();
+        }
+
+        public bool AddDemandResources(ResourceType resource)
+        {
+            if (buildingUpgraderModel.AddResource(resource))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool ProduceResource()
+        {
+            resourceAmount++;
+            return true;
+        }
+    }
+}
