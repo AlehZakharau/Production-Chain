7998478e22eb64d56dfe717d0131df40626eadc4
Bezymyanyj <Bezymyanyj@gmail.com>
2021-12-04 18:02:42 -0500

    Tower Model

---
+++ Assets/GameLogic/Buildings/Models/TowerModel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace GameLogic.Manufacture
+{
+    public class TowerModel : IBuildingModel
+    {
+        public BuildingsType BuildingsType { get; set; }
+        public Vector3 Position { get; set; }
+        
+        private readonly IBuildingUpgraderModel buildingUpgraderModel;
+        
+        public TowerModel(IBuildingUpgraderModel buildingUpgraderModel,
+            InitializeData.InitData initData)
+        {
+            this.buildingUpgraderModel = buildingUpgraderModel;
+
+            BuildingsType = initData.buildingsType;
+            Position = initData.Position.position;
+            
+            buildingUpgraderModel.OnUpgrade += OnUpgrade;
+        }
+
+        private void OnUpgrade()
+        {
+            // Open new Area
+            throw new System.NotImplementedException();
+        }
+    }
+}
