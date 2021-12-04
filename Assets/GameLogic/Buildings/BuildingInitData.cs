6ff495c920583d1bc63e8985934770b3939e874f
Bezymyanyj <Bezymyanyj@gmail.com>
2021-12-04 17:59:33 -0500

    Building Initiate Data

---
+++ Assets/GameLogic/Buildings/BuildingInitData.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameLogic.Manufacture
+{
+    public class BuildingInitData : MonoBehaviour
+    {
+        [SerializeField] private InitializeData.InitData initData;
+
+        [SerializeField] private InitializeData.ManufactureInitData manufactureInitData;
+        [SerializeField] private InitializeData.RefineryInitData refineryInitData;
+        [SerializeField] private InitializeData.LevelInitData[] levelsInitData =
+            new[] { new InitializeData.LevelInitData(), new InitializeData.LevelInitData()};
+
+        public InitializeData.InitData InitData => initData;
+        public InitializeData.ManufactureInitData ManufactureInitData => manufactureInitData;
+        public InitializeData.RefineryInitData RefineryInitData => refineryInitData;
+        public InitializeData.LevelInitData[] LevelInitData => levelsInitData;
+        //public ManufactureData ManufactureData => new ManufactureData(initData, levelsInitData);
+
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            CheckInitData();
+        }
+        private void CheckInitData()
+        {
+            if (refineryInitData.demandProducingResources.Count < 1)
+            {
+                Debug.LogWarning($"<color=blue>Init Data</color>" +
+                                 $"Demand Producing resources for {this} is empty");
+            }
+
+            foreach (var levelData in levelsInitData)
+            {
+                if (levelData.demandUpgradeResource.Count < 1)
+                {
+                    Debug.LogWarning($"<color=blue>Init Data</color>" +
+                                     $"Demand Upgrade resources for {this} is empty");
+                }
+
+                if (levelData.demandUpgradeResource.Count !=
+                    levelData.demandUpgradeResourceCapacity.Length)
+                {
+                    Debug.LogWarning($"<color=blue>Init Data</color>" +
+                                     $"Demand Upgrade resources or Capacity has mistake {this}");
+                }
+
+                foreach (var capacity in levelData.demandUpgradeResourceCapacity)
+                {
+                    if (capacity == 0)
+                        Debug.LogWarning($"<color=blue>Init Data</color>" +
+                                         $"Demand Upgrade resources capacity is 0 {this}");
+                }
+            }
+        }
+#endif
+    }
+    
+    [Serializable]
+    public class InitializeData
+    {
+        [Serializable]
+        public struct InitData
+        {
+            public BuildingsType buildingsType;
+            public Transform Position;
+        }
+        [Serializable]
+        public struct LevelInitData
+        {
+            public  List<ResourceType> demandUpgradeResource;
+            public  int[] demandUpgradeResourceCapacity;
+            public  float upgradeProductionSpeedCoefficient;
+        }
+        [Serializable]
+        public struct RefineryInitData
+        {
+            public List<ResourceType> demandProducingResources;
+        }
+        [Serializable]
+        public struct ManufactureInitData
+        {
+            public ResourceType resourceType;
+            public  float productionSpeed;
+            public  Color color;
+        }
+    }
+}
