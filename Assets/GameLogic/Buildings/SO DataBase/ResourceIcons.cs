3cba125683a66569cd60229d0289f6686bcec70d
Bezymyanyj <Bezymyanyj@gmail.com>
2021-12-16 05:14:37 -0500

    Icons DB

---
+++ Assets/GameLogic/Buildings/SO DataBase/ResourceIcons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameLogic.Manufacture
+{
+    [CreateAssetMenu(fileName = "ResourceIcons", menuName = "DB/ResourceIcons", order = 0)]
+    public class ResourceIcons : ScriptableObject
+    {
+        [SerializeField] private ResourceType[] resourceTypes;
+        [SerializeField] private Sprite[] resourceIcons;
+
+        public Dictionary<ResourceType, Sprite> resourceIconsDB = new Dictionary<ResourceType, Sprite>();
+
+        private void Awake()
+        {
+            if (resourceTypes.Length != resourceIcons.Length)
+            {
+                Debug.LogError($"Init Data has mistake {this.name}");
+            }
+            for (var i = 0; i < resourceTypes.Length; i++)
+            {
+                resourceIconsDB.Add(resourceTypes[i], resourceIcons[i]);
+            }
+        }
+    }
+}
