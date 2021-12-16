07b9318883e99c658d860591f28e357500ba1623
Bezymyanyj <Bezymyanyj@gmail.com>
2021-12-16 05:13:29 -0500

    Clicable View

---
+++ Assets/GameLogic/Buildings/Views/BuildingClicable.cs
@@ -0,0 +1,24 @@
+﻿using GameLogic.CameraController;
+using UnityEngine;
+
+namespace GameLogic.Manufacture
+{
+    public class BuildingClicable : MonoBehaviour, IClickable
+    {
+        [SerializeField] private GameObject hidPanel;
+        public void Click()
+        {
+            Debug.Log($"Click {this.gameObject.name}");
+        }
+
+        public void Select()
+        {
+            hidPanel.SetActive(true);
+        }
+
+        public void UnSelect()
+        {
+            hidPanel.SetActive(false);
+        }
+    }
+}
